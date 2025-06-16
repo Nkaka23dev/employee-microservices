ebfb1caf4e409743bcda4ea47c6415bd363c74c6
ericnkaka <nkakaeric96@gmail.com>
2025-06-16 15:58:31 +0200

    feat: adding Dockerfile and creatign image for Payroll service

--- PayrollService/Program.cs
+++ PayrollService/Program.cs
@@ -3,22 +3,22 @@ using PayrollService.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(option => option.UseInMemoryDatabase("inMem"));
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddControllers();
-
 builder.Services.AddScoped<IPayrollRepo, PayrollRepo>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseHttpsRedirection(); // keep this in dev only
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection(); // optional: comment out for Docker if needed
+
 app.MapControllers();
 
 PrepDb.PrepPopulation(app);
